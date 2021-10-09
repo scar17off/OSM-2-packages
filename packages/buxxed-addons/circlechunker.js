72bba02577cc129a79b1c23057b440bea61879dc
scar17off <81940406+scar17off@users.noreply.github.com>
2021-10-09 14:19:43 +0300

    Update circlechunker.js

--- packages/buxxed-addons/circlechunker.js
+++ packages/buxxed-addons/circlechunker.js
@@ -1,4 +1,4 @@
-OWOP.tool.addToolObject(new OWOP.tool.class('addons/BotCircleChunker64.js', OWOP.cursors.erase, OWOP.fx.player.RECT_SELECT_ALIGNED(16), false, function(tool) {
+OWOP.tool.addToolObject(new OWOP.tool.class('addons/BotCircleChunker16.js', OWOP.cursors.erase, OWOP.fx.player.RECT_SELECT_ALIGNED(16), false, function(tool) {
                         let pix = 16;
                         let index = 0;
                         tool.setEvent('mousemove mousedown', async mouse => {
